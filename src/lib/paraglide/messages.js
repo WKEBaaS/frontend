60c8ad45d08289e25d02308dc4239cb8fef24dc9
youmin1017 <youmin1017@wke.csie.ncnu.edu.tw>
2024-10-26 22:34:51 +0800

    add dashboard layout

--- src/lib/paraglide/messages.js
+++ src/lib/paraglide/messages.js
@@ -1,24 +1,48 @@
 /* eslint-disable */
-import { languageTag } from './runtime.js';
-import * as en from './messages/en.js';
-import * as zh_tw from './messages/zh-tw.js';
+import { languageTag } from "./runtime.js"
+import * as en from "./messages/en.js"
+import * as zh_tw from "./messages/zh-tw.js"
+
 
 /**
  * This message has been compiled by [inlang paraglide](https://inlang.com/m/gerre34r/library-inlang-paraglideJs).
  *
  * - Don't edit the message's code. Use [Sherlock (VS Code extension)](https://inlang.com/m/r7kp499g/app-inlang-ideExtension),
  *   the [web editor](https://inlang.com/m/tdozzpar/app-inlang-finkLocalizationEditor) instead, or edit the translation files manually.
- *
+ * 
  * - The params are NonNullable<unknown> because the inlang SDK does not provide information on the type of a param (yet).
- *
+ * 
  * @param {{ name: NonNullable<unknown> }} params
  * @param {{ languageTag?: "en" | "zh-tw" }} options
  * @returns {string}
  */
 /* @__NO_SIDE_EFFECTS__ */
-export const hello_world = (params, options = {}) => {
+export const hello_world = (params , options = {}) => {
 	return {
 		en: en.hello_world,
-		'zh-tw': zh_tw.hello_world,
-	}[options.languageTag ?? languageTag()](params);
-};
+		"zh-tw": zh_tw.hello_world
+	}[options.languageTag ?? languageTag()](params)
+}
+
+
+
+/**
+ * This message has been compiled by [inlang paraglide](https://inlang.com/m/gerre34r/library-inlang-paraglideJs).
+ *
+ * - Don't edit the message's code. Use [Sherlock (VS Code extension)](https://inlang.com/m/r7kp499g/app-inlang-ideExtension),
+ *   the [web editor](https://inlang.com/m/tdozzpar/app-inlang-finkLocalizationEditor) instead, or edit the translation files manually.
+ * 
+ * - The params are NonNullable<unknown> because the inlang SDK does not provide information on the type of a param (yet).
+ * 
+ * @param {{}} params
+ * @param {{ languageTag?: "en" | "zh-tw" }} options
+ * @returns {string}
+ */
+/* @__NO_SIDE_EFFECTS__ */
+export const projects = (params = {}, options = {}) => {
+	return {
+		en: en.projects,
+		"zh-tw": zh_tw.projects
+	}[options.languageTag ?? languageTag()]()
+}
+
---
+++ src/routes/dashboard/header.svelte
@@ -0,0 +1,15 @@
+<script>
+	import LanguageSwitch from '$lib/components/language-switch.svelte';
+	import ThemeSwitcher from '$lib/components/theme-switch.svelte';
+	import * as Sidebar from '$lib/components/ui/sidebar';
+</script>
+
+<header
+	class="inline-flex items-center justify-between border-border/40 bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur"
+>
+	<Sidebar.Trigger />
+	<div>
+		<LanguageSwitch />
+		<ThemeSwitcher />
+	</div>
+</header>
